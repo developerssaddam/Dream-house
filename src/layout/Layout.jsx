55c91d2723928131c29b180c1ca8676933b591dc
developersaddam <developerssaddam@gmail.com>
2024-04-08 01:53:38 +0600

    Setup project invironment and routes

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer/Footer";
+import NavBar from "../components/NavBar/NavBar";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/router/router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/login",
+        element: <Login />,
+      },
+
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+export default router;
